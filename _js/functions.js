f8d96fce3291fa527c7ad5b61c35f674d508eb8a
Alcenir Costa <alcenir.1994@gmail.com>
2020-04-15 12:08:49 -0300

    Initial commit

---
+++ _js/functions.js
@@ -0,0 +1,10 @@
+		$(function(){
+			$('#menu-mobile').click(function(){
+				var el = $(this).parent().find('ul');
+				if(el.is(':visible') == false){
+					el.fadeIn();
+				}else{
+					el.fadeOut();
+				}
+			});
+		});
---
+++ index.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>Projeto 06</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+	<link href="_css/style.css" rel="stylesheet">
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width;initial-scale=1.0;maximum-scale=1.0">
+	<meta name="desciption" content="descrição do meu site"/>
+	<meta name="keywords" content="palavras chave de busca"/>
+	<meta name="author" content="Alcenir da Costa"/>
+</head>
+<body>
+	<section class="main">
+		<div class="layer"></div><!--layer-->
+		<div class="center">
+			<header>
+				<div class="icons-social">
+					<a href="https://facebook.com"><i class="fab fa-facebook-f"></i></a>
+					<a href="https://instagram.com"><i class="fab fa-instagram"></i></a>
+				</div><!--icons-social-->
+				<div class="clear"></div><!--clear-->
+				<div class="logo">
+					Logo/Empresa
+				</div><!--logo-->
+				<nav class="desktop">
+					<ul>
+						<li><a href="">Home</a></li>
+						<li><a href="">Sobre</a></li>
+						<li><a href="">Contato</a></li>
+					</ul>
+				</nav>
+				<nav class="mobile">
+					<i id="menu-mobile" class="fas fa-bars"></i>
+					<ul>
+						<li><a href="">Home</a></li>
+						<li><a href="">Sobre</a></li>
+						<li><a href="">Contato</a></li>
+					</ul>
+				</nav><!--mobile-->
+				<div class="clear"></div><!--clear-->
+			</header>
+			<div class="conteudo-header">
+				<h2>Bem-vindo(a) ao nosso <span>Web-Site!</span></h2>
+				<p>Sed ut ornare erat, in efficitur orci. Fusce nunc erat, ultricies at massa nec, hendrerit ullamcorper diam. Ut quis vehicula turpis. Nullam tempor lacus lacinia magna tempor, eu tincidunt ipsum lobortis. Donec s
+				ed placerat lacus, at tincidunt dolor.</p>
+				<p>In laoreet commodo neque, in varius orci pharetra et. Donec sagittis nulla a porta scelerisque. Nulla dapibus lobortis velit. Nulla consectetur nunc in ante congue egestas. Sed sollicitudin, mi ut mattis facilisis, arcu purus euismod nisi, non convallis enim nulla nec mauris. In cursus eu nulla ac tempor. Phasellus hendrerit, libero in lacinia pellentesque, neque ante egestas est, ac cursus augue nulla ac ante. Vivamus eget rutrum elit, dapibus venenatis ante. Sed elit dui, fermentum a imperdiet id, hendrerit et lorem.</p>
+				<a href="">Saiba mais!</a>
+			</div><!--conteudo-header-->
+		</div><!--center-->
+	</section><!--main-->
+	<section class="planos">
+		<div class="chamada-planos">
+			<div class="center">
+				<h2>Confira nossos planos!</h2>
+			</div><!--center-->
+		</div><!--chamada-planos-->
+		<section class="box-planos">
+			<div class="center">
+				<div class="box-planos-single">
+					<div class="box-planos-wraper">
+						<h2>Lorem Ipsun JR</h2>
+						<div class="pontos-plano">
+							<p><i style="color: green;" class="fa fa-check-circle"></i> Lorem Ipsun slior domor</p>
+							<p><i style="color: red;" class="fas fa-exclamation-circle"></i> Lorem Ipsun slior domor</p>
+							<p><i style="color: red;" class="fas fa-exclamation-circle"></i> Lorem Ipsun slior domor</p>
+							<p><i style="color: red;" class="fas fa-exclamation-circle"></i> Lorem Ipsun slior domor</p>
+						</div><!--pontos-plano-->	
+						<div class="preco">
+							<h2>R$: 100,00</h2>
+						</div><!--preco-->		
+					</div><!--box-planos-wraper-->			
+				</div><!--box-planos-single-->
+				<div class="box-planos-single">
+					<div class="box-planos-wraper">
+						<h2>Lorem Ipsun PLENO</h2>
+						<div class="pontos-plano">
+							<p><i style="color: green;" class="fa fa-check-circle"></i> Lorem Ipsun slior domor</p>
+							<p><i style="color: green;" class="fa fa-check-circle"></i> Lorem Ipsun slior domor</p>
+							<p><i style="color: red;" class="fas fa-exclamation-circle"></i> Lorem Ipsun slior domor</p>
+							<p><i style="color: red;" class="fas fa-exclamation-circle"></i> Lorem Ipsun slior domor</p>
+						</div><!--pontos-plano-->	
+						<div class="preco">
+							<h2>R$: 150,00</h2>
+						</div><!--preco-->		
+					</div><!--box-planos-wraper-->			
+				</div><!--box-planos-single-->
+				<div class="box-planos-single">
+					<div class="box-planos-wraper">
+						<h2>Lorem Ipsun SENIOR (Full)</h2>
+						<div class="pontos-plano">
+							<p><i style="color: green;" class="fa fa-check-circle"></i> Lorem Ipsun slior domor</p>
+							<p><i style="color: green;" class="fa fa-check-circle"></i> Lorem Ipsun slior domor</p>
+							<p><i style="color: green;" class="fa fa-check-circle"></i> Lorem Ipsun slior domor</p>
+							<p><i style="color: green;" class="fa fa-check-circle"></i> Lorem Ipsun slior domor</p>
+						</div><!--pontos-plano-->	
+						<div class="preco">
+							<h2>R$: 250,00</h2>
+						</div><!--preco-->	
+					</div><!--box-planos-wraper-->			
+				</div><!--box-planos-single-->
+			</div><!--center-->
+		</section><!--box-planos-->
+	</section><!--planos-->
+	<section class="parallax">
+		<div class="overlay-parallax"></div>
+		<div class="center">
+			<h2>Sobre nossa <span style="color: #2692FF;">empresa</span></h2>
+			<p>Sed ut ornare erat, in efficitur orci. Fusce nunc erat, ultricies at massa nec, hendrerit ullamcorper diam. Ut quis vehicula turpis. Nullam tempor lacus lacinia magna tempor, eu tincidunt ipsum lobortis. Donec sed placerat lacus, at tincidunt dolor.</p>
+			<p>In laoreet commodo neque, in varius orci pharetra et. Donec sagittis nulla a porta scelerisque. Nulla dapibus lobortis velit. Nulla consectetur nunc in ante congue egestas. Sed sollicitudin, mi ut mattis facilisis, arcu purus euismod nisi, non convallis enim nulla nec mauris. In cursus eu nulla ac tempor. Phasellus hendrerit, libero in lacinia pellentesque, neque ante egestas est, ac cursus augue nulla ac ante. Vivamus eget rutrum elit, dapibus venenatis ante. Sed elit dui, fermentum a imperdiet id, hendrerit et lorem.</p>
+		</div><!--center-->
+	</section><!--parallax-->
+	<section class="final-section">
+		<div class="center">
+			<div class="w50 contato">
+				<h2><div class="borda-efeito"></div>Entre em contato</h2>
+				<form>
+					<input required type="text" name="" placeholder="Insira seu NOME">
+					<input required type="email" name="" placeholder="Insira seu E-mail">
+					<textarea placeholder="Deixe sua Mensagem"></textarea>
+					<input type="submit" name="" value="Enviar">
+				</form>
+			</div><!--w50-->
+			<div class="w50 time">
+				<h2><div class="borda-efeito"></div> Conheça nosso time</h2>
+				<div class="w50 equipe-single">
+					<div class="topo-box-usuario">
+						<div class="box-avatar"></div><div class="info-usuario">
+							<p style="font-weight: bold;">Nome Sobrenome</p>
+							<p style="font-weight: italic;">CEO/INSTRUTOR</p>
+						</div>
+					</div><!--topo-box-usuario-->
+					<div class="descricao-usuario-box">
+						<p>Sed sollicitudin, mi ut mattis facilisis, arcu purus euismod nisi, non convallis enim nulla nec mauris. In cursus eu nulla ac tempor. Phasellus hendrerit, libero in lacinia pellentesque, neque ante egestas est, ac cursus augue nulla ac ante. Vivamus eget rutrum elit, dapibus venenatis ante.</p>
+					</div><!--descricao-usuario-box-->
+				</div><!--equipe-single-->
+				<div class="w50 equipe-single">
+					<div class="topo-box-usuario">
+						<div class="box-avatar"></div><div class="info-usuario">
+							<p style="font-weight: bold;">Nome Sobrenome</p>
+							<p style="font-weight: italic;">CEO/INSTRUTOR</p>
+						</div>
+					</div><!--topo-box-usuario-->
+					<div class="descricao-usuario-box">
+						<p>Sed sollicitudin, mi ut mattis facilisis, arcu purus euismod nisi, non convallis enim nulla nec mauris. In cursus eu nulla ac tempor. Phasellus hendrerit, libero in lacinia pellentesque, neque ante egestas est, ac cursus augue nulla ac ante. Vivamus eget rutrum elit, dapibus venenatis ante.</p>
+					</div><!--descricao-usuario-box-->
+				</div><!--equipe-single-->
+				<div class="w50 equipe-single">
+					<div class="topo-box-usuario">
+						<div class="box-avatar"></div><div class="info-usuario">
+							<p style="font-weight: bold;">Nome Sobrenome</p>
+							<p style="font-weight: italic;">CEO/INSTRUTOR</p>
+						</div>
+					</div><!--topo-box-usuario-->
+					<div class="descricao-usuario-box">
+						<p>Sed sollicitudin, mi ut mattis facilisis, arcu purus euismod nisi, non convallis enim nulla nec mauris. In cursus eu nulla ac tempor. Phasellus hendrerit, libero in lacinia pellentesque, neque ante egestas est, ac cursus augue nulla ac ante. Vivamus eget rutrum elit, dapibus venenatis ante.</p>
+					</div><!--descricao-usuario-box-->
+				</div><!--equipe-single-->
+				<div class="w50 equipe-single">
+					<div class="topo-box-usuario">
+						<div class="box-avatar"></div><div class="info-usuario">
+							<p style="font-weight: bold;">Nome Sobrenome</p>
+							<p style="font-weight: italic;">CEO/INSTRUTOR</p>
+						</div>
+					</div><!--topo-box-usuario-->
+					<div class="descricao-usuario-box">
+						<p>Sed sollicitudin, mi ut mattis facilisis, arcu purus euismod nisi, non convallis enim nulla nec mauris. In cursus eu nulla ac tempor. Phasellus hendrerit, libero in lacinia pellentesque, neque ante egestas est, ac cursus augue nulla ac ante. Vivamus eget rutrum elit, dapibus venenatis ante.</p>
+					</div><!--descricao-usuario-box-->
+				</div><!--equipe-single **  Classe usada para cada membro da equipe!! -->
+				<div class="clear"></div>
+			</div><!--w50-->
+			<div class="clear"></div>
+		</div><!--center-->
+	</section><!--final-section-->
+	<footer>
+		<p>Todos os direitos reservados ao PROJETO #6!!</p>
+	</footer><!--footer-->
+	<script src="_js/jquery.js"></script>
+	<script src="_fontawesome/js/all.js"></script>
+	<script src="_fontawesome/js/all.js"></script>
+	<script src="_js/all.min.js"></script>
+</body>
+</html>
